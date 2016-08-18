c2c5dae31af75a105d46502b1d013c004c7f0f98
Daniel Fritsch <danielfritsch@siemens.com>
2016-08-18 10:30:23 +0200

    Minimal Example for potential sprockets-rails issue

---
+++ vendor/assets/javascripts/svm-workspace/build/temp/development/ThemeDemoApp/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../theme-demo-app/sass/config.rb')
+cache_path = '/home/dev/RubymineProjects/svmportal/vendor/assets/javascripts/extjs/svm-workspace/build/.sass-cache'
---
+++ vendor/assets/javascripts/svm-workspace/build/temp/development/ThemeDemoApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /home/dev/RubymineProjects/svmportal/vendor/assets/javascripts/extjs/svm-workspace/theme-demo-app/app.js
